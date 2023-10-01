1d59b0de80c3469ba9fbac7838b24b76f2eb934a
Gerardo <gmayboca@gmail.com>
2023-09-30 18:44:56 -0700

    Installed prettier formatter for tailwindcss

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,25 +1,24 @@
-"use client"
+"use client";
 
-import { Text, useTheme } from "@nextui-org/react";
-
+import { Spacer, colors } from "@nextui-org/react";
+import Image from "next/image";
 
 const Navbar = () => {
-
-    const { theme } = useTheme();
-
-    return (
-        <div className="" style={{
-            display: "flex",
-            width: "100%",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "start",
-            padding: "0 20px",
-            backgroundColor: theme?.colors.gray900
-        }}>
-            <Text>Hola Mundo</Text>
-        </div>
-    )
-}
+  return (
+    <div className="flex w-full flex-row items-center justify-start bg-zinc-900 px-4 py-1 ">
+      <Image
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+        alt="ditto"
+        width={70}
+        height={70}
+      />
+      <p className="text-xl">
+        <span className="text-2xl">P</span>okemon
+      </p>
+      <Spacer className="flex-1" />
+      <p>Favoritos</p>
+    </div>
+  );
+};
 
 export default Navbar;
