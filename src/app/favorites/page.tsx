ea6258f0fed3dc2b7ab1d44495bdc171d85b576e
Gerardo <gmayboca@gmail.com>
2023-10-09 22:40:52 -0700

    Created Favorite page and updated Navbar with Link components

---
+++ src/app/favorites/page.tsx
@@ -0,0 +1,9 @@
+const FavoritesPage = () => {
+  return (
+    <main>
+        <h1>This is the favorites Page</h1>
+    </main>
+  )
+}
+
+export default FavoritesPage;
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <div className="flex w-full flex-row items-center justify-start bg-zinc-900 px-6 py-4 ">
-      <p className="text-xl mr-auto">
-        <span className="text-2xl font-bold">P</span>okemon
-      </p>
-      <p>Favoritos</p>
+      <Link className="mr-auto text-xl" href="/">
+        <p>
+          <span className="text-2xl font-bold">P</span>okemon
+        </p>
+      </Link>
+      <Link href="/favorites">
+        Favorites
+      </Link>
     </div>
   );
 };
