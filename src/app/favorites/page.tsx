e60d88701ab7f76c91f7cc53b2404083a8b41be2
Gerardo <gmayboca@gmail.com>
2023-10-11 21:20:07 -0700

    Updated favorite page with logic to avoid localStorage undefined error

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -3,9 +3,14 @@
 import { getFavoritePokemons } from "@/utils";
 import FavoritePokemonsGrid from "./components/favorite-pokemons-grid";
 import NoPokemonsPlaceholder from "./components/no-pokemons-placeholder";
+import { useEffect, useState } from "react";
 
 const FavoritesPage = () => {
-  const favoritePokemons = getFavoritePokemons();
+  const [favoritePokemons, setFavoritePokemons] = useState<string[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemons(getFavoritePokemons())
+  }, []);
 
   return (
     <main className="p-4">
