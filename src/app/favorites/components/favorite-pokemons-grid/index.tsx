e9379f205a4c3b906ac0b38b95fa709e28d5e559
Gerardo <gmayboca@gmail.com>
2023-10-11 20:53:49 -0700

    onClick functionality for pokemon cards inside favorite page

--- src/app/favorites/components/favorite-pokemons-grid/index.tsx
+++ src/app/favorites/components/favorite-pokemons-grid/index.tsx
@@ -1,25 +1,36 @@
 import { Card, CardBody } from "@nextui-org/card";
 import { Image } from "@nextui-org/image";
+import { useRouter } from "next/navigation";
 
 interface Props {
-    pokemons: string[]
+  pokemons: string[];
 }
 
 const FavoritePokemonsGrid = ({ pokemons }: Props) => {
+
+  const router = useRouter();
+
+  const onClick = (pokemonId: string) => {
+    router.push("/pokemon/" + pokemonId);
+  }
+
   return (
-    <div className="xs:grid-flow-col-1 grid gap-4 md:grid-cols-4 lg:grid-cols-8">
-      {pokemons.map((pokemon) => (
-        <Card key={pokemon}>
-          <CardBody>
-            <Image
-              alt="pokemon"
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon}.svg`}
-              width="100%"
-            />
-          </CardBody>
-        </Card>
-      ))}
-    </div>
+    <>
+      <h1 className="mb-4 text-4xl font-bold">Favorites</h1>
+      <div className="xs:grid-flow-col-1 grid gap-4 md:grid-cols-4 lg:grid-cols-8">
+        {pokemons.map((pokemon) => (
+          <Card isPressable key={pokemon} onClick={() => onClick(pokemon)}>
+            <CardBody>
+              <Image
+                alt="pokemon"
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon}.svg`}
+                width="100%"
+              />
+            </CardBody>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 };
 
